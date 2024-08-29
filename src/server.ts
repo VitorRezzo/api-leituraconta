f4de59127e8992e79f19bb49500b3bc0bd1fa9a2
Vitor Rezzo <cv.rezz99@gmail.com>
2024-08-28 22:18:30 -0300

    feat: adicionado arquivo server com express , http ,dotenv e tipagem

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const HOST = process.env.HOST || "localhost";
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Servidor ok");
+});
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Servidor Rodando http://${HOST}:${PORT}/`);
+});
